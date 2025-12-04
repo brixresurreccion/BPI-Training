d74c2db8512544f587f16f84a57fe5af221026aa
Brix P. Resurreccion <bpresurreccion@headoffice.corp.bpi.com.ph>
2025-12-04 14:21:37 +0800

    comment

---
+++ Activity1.java
@@ -0,0 +1,18 @@
+package com.bpi.helloworld.main;
+
+import java.util.Scanner;
+
+public class Activity1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter Name?");
+		String name = input.next();
+		
+		
+		System.out.println("Hello, " + name + "!");
+	}
+
+}
