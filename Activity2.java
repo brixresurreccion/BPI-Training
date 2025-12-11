b1e6fd74784a251d44cb261afb930c3549f5bf1f
Brix P. Resurreccion <bpresurreccion@headoffice.corp.bpi.com.ph>
2025-12-11 11:11:44 +0800

    M2_Activity2

---
+++ Activity2.java
@@ -0,0 +1,12 @@
+package M2_Activity2;
+
+public class Activity2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		whiteboard wb = new whiteboard();
+		int sum = wb.addnumbers(11,11);
+		System.out.println("Total: " + (sum));
+	}
+
+}
---
+++ whiteboard.java
@@ -0,0 +1,8 @@
+package M2_Activity2;
+
+public class whiteboard {
+	int addnumbers(int num1, int num2) {
+		int sum = num1 + num2;
+		return sum;
+	}
+}
